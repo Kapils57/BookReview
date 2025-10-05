44a173d2a87d1f240134e105431f2550e106c68b
Kapils57 <kapils1646@gmail.com>
2025-10-05 13:41:06 +0530

    Update Login.jsx

--- bookreview/src/pages/Login.jsx
+++ bookreview/src/pages/Login.jsx
@@ -32,7 +32,7 @@ function Login({setIsLoggedIn}) {
 
 
 
-        let response = await fetch("http://localhost:5000/api/users/login", {
+        let response = await fetch("https://bookreview3-backend.onrender.com/api/users/login", {
             method:"POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
